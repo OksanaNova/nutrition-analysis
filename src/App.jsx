e70febf679641a9cbb631913f4c83ca120d2d4cf
OksanaNova <o.nova0804@gmail.com>
2025-01-24 11:59:22 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import { useEffect } from 'react';
+import './App.css'
+
+function App() {
+
+  // https://api.edamam.com/api/nutrition-data?app_id=1f7bf3d0&app_key=%20c96484924128aeb3cec27d734466aae1&nutrition-type=cooking&ingr=avocado
+
+  const MY_KEY = 'c96484924128aeb3cec27d734466aae1';
+  const MY_ID = '1f7bf3d0';
+  const MY_URL = 'https://api.edamam.com/api/nutrition-details';
+
+  const getNutrition = 
+
+
+
+  return (
+    <>
+
+    
+
+    </>
+  )
+}
+
+export default App
