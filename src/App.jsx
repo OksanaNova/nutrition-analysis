fd4d7ec039d0e35a152e865707b6c0a868a7c0f7
OksanaNova <o.nova0804@gmail.com>
2025-01-27 11:57:41 -0700

    fetch request

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,38 @@ function App() {
   const MY_ID = '1f7bf3d0';
   const MY_URL = 'https://api.edamam.com/api/nutrition-details';
 
+  const fetchData = async (ingr) => {
+    setStateLoader(true);
+
+    const response = await fetch(`${MY_URL}?app_id=${MY_ID}&app_key=${MY_KEY}`, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ ingr: ingr })
+    })
+
+    if(response.ok) {
+      setStateLoader(false);
+      const data = await response.json();
+      setMyNutrition(data);
+    } else {
+      setStateLoader(false);
+      alert('ingredients entered incorrectly')
+    }
+  }
+
+  useEffect(() => {
+    let ingr = ['avocado'];
+    fetchData(ingr)
+  })
+
+
+ 
+
+
+
 
 
 
