9644f6c3e0a01ceb5ea7ec8da6918c91aed75d32
OksanaNova <o.nova0804@gmail.com>
2025-01-27 10:28:36 -0700

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
+import LoaderPage from './LoaderPage';
 
 function App() {
 
   // https://api.edamam.com/api/nutrition-data?app_id=1f7bf3d0&app_key=%20c96484924128aeb3cec27d734466aae1&nutrition-type=cooking&ingr=avocado
 
+  const [myNutrition, setMyNutrition] = useState();
+  const [stateLoader, setStateLoader] = useState(false);
+
   const MY_KEY = 'c96484924128aeb3cec27d734466aae1';
   const MY_ID = '1f7bf3d0';
   const MY_URL = 'https://api.edamam.com/api/nutrition-details';
 
-  const getNutrition = 
+
 
 
 
   return (
     <>
+    {stateLoader && <LoaderPage />}
 
     
 
